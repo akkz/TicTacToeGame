e4fd31e200874f50bad3b31b3677cd06ad448e21
akkz <work@akkz.net>
2023-05-21 16:37:40 +0800

    打包问题处理

--- Source/TicTacToe/TicTacToeGameMode.cpp
+++ Source/TicTacToe/TicTacToeGameMode.cpp
@@ -11,14 +11,14 @@ ATicTacToeGameMode::ATicTacToeGameMode()
 	PlayerControllerClass = ATicTacToePlayerController::StaticClass();
 
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/TopDown/Blueprints/BP_TopDownCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Gameplay/Player/BP_ChessPlayer"));
 	if (PlayerPawnBPClass.Class != nullptr)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
 	}
 
 	// set default controller to our Blueprinted controller
-	static ConstructorHelpers::FClassFinder<APlayerController> PlayerControllerBPClass(TEXT("/Game/TopDown/Blueprints/BP_TopDownPlayerController"));
+	static ConstructorHelpers::FClassFinder<APlayerController> PlayerControllerBPClass(TEXT("/Game/Gameplay/Player/BP_ChessController"));
 	if(PlayerControllerBPClass.Class != NULL)
 	{
 		PlayerControllerClass = PlayerControllerBPClass.Class;
